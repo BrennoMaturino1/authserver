4e47345547ffb63d57311133b978e708a9de5ab5
Brenno <67515570+BrennoMaturino1@users.noreply.github.com>
2024-05-10 20:54:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ app.use(express.json());
 
 // POST endpoint
 app.post("/", (req, res) => {
-    const key = req.query.key;
+    const { key } = req.query;
+    const [usernameKey, codeKey] = process.env.USERNAME_CODE.split(", ");
 
-    if (process.env[key]) {
-        res.status(200).json({ userName: process.env[key] });
+    if (process.env[usernameKey] && process.env[codeKey]) {
+        const username = process.env[usernameKey];
+        const code = process.env[codeKey];
+
+        if (key === code) {
+            res.status(200).json({ userName: username });
+        } else {
+            res.status(401).json({ userName: username });
+        }
     } else {
-        res.status(401);
+        res.status(500).send("Environment variables not properly configured.");
     }
 });
 
