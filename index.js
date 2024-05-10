b0e9ff8acf605dee94ffe8812301a80f2611d843
Brenno <67515570+BrennoMaturino1@users.noreply.github.com>
2024-05-10 20:59:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// Middleware to serve index.html for GET requests to root
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 // POST endpoint
 app.post("/", (req, res) => {
