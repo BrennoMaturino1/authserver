0799ab023e1b3be249f52d8b22f603e505b66308
Brenno <67515570+BrennoMaturino1@users.noreply.github.com>
2024-05-10 22:20:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 const express = require("express");
 require("dotenv").config();
-const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1024;
 
 app.use(express.json());
 
-// Middleware to serve index.html for GET requests to root
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
 // POST endpoint
 app.post("/", (req, res) => {
-    const [usernameKey, codeKey] = process.env.[req.query].split(", ");
+    processReq(req, res, false)
+});
 
-    if (process.env[usernameKey] && process.env[codeKey]) {
-        const username = process.env[usernameKey];
-        const code = process.env[codeKey];
+app.get("/", (req, res) => {
+    processReq(req, res, true)
+});
 
-        if (key === code) {
-            res.status(200).json({ userName: username });
-        } else {
-            res.status(401).json({ userName: username });
+function processReq(req, res, isGet) {
+
+    if (isGet) {
+        console.warn("A request was made with isGet set to true, you should be aware that get requests are for quickly testing the API in the search bar and shouldn't be used for production")
+    }
+
+    if (!req.query.key) {
+        res.status(400).json("§4Bad request, key is invalid§r");
+        console.error("a client sent a bad request OwO")
+    }
+
+    if (req.query.key in process.env) {
+        const [username, accessCode] = process.env[req.query.key].split(", ")
+
+        if (accessCode == "OK") {
+            console.log(username + " (or someone with their acess key) logged in sucessfully UwU :3")
+            res.status(200).json({ userName: username, message: "Welcome, §p" + username + "§r, your account has been verified §asucessfully§r" })
+        } else if (accessCode == "SUSP") {
+            console.log(username + " (or someone with their acess key) tried to log in but their account is suspended xD")
+            res.status(401).json({ userName: username, message: "Welcome, §p" + username + "§r, your account has been §4suspended§r and you cannot use this application" })
         }
     } else {
-        res.status(500).send("Environment variables not properly configured.");
+        console.log("Someone tried to log in with an invalid key")
+        res.status(401).json({ message: "§4Invalid§r access key" })
     }
-});
+    console.error("i dunno what happened OwO")
+    res.status(400).json({ message: "§4Bad request, unknown error§r"});
+}
 
 // Start the server
 app.listen(port, () => {
