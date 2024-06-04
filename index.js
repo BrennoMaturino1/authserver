38622c7f365fdc9b62d5271b6201c89a237e5ce7
Brenno <67515570+BrennoMaturino1@users.noreply.github.com>
2024-06-04 14:53:34 -0300

    Update index.js
    add body parser and update version number

--- index.js
+++ index.js
@@ -5,86 +5,86 @@ const app = express();
 const port = process.env.PORT || 80;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // POST endpoint
 app.post("/", (req, res) => {
-    processReq(req, res, false)
+    processReq(req, res, false);
 });
 
 app.get("/", (req, res) => {
-    processReq(req, res, true)
-    //res.status(400).json({ status: "ClientError", message: "Bad Request, GET Requests are not allowed" });
+    processReq(req, res, true);
 });
 
 function processReq(req, res, isGet) {
-
     if (isGet) {
-        console.warn("A GET request was made!!!!")
+        console.warn("A GET request was made!!!!");
     }
 
     console.log(req.headers);
-    console.log(req.body)
+    console.log(req.body);
 
     if (!req.body.key) {
         res.status(400).json({ status: "ClientError", message: "§4Bad request, key is invalid§r" });
-        console.error("a client sent a bad request OwO [" + req.ip + "]")
+        console.error("a client sent a bad request OwO [" + req.ip + "]");
         return;
     }
 
     console.log(req.ip + " sent a request: ?key=" + req.query.key);
 
-    if (req.body.key in process.env) {
-        const [username, accessCode] = process.env[req.body.key].split(",")
+    const userKey = req.body.key;
+    if (userKey in process.env) {
+        const [username, accessCode] = process.env[userKey].split(",");
+
         console.log(username + " //// " + accessCode);
 
         if (accessCode === "OK") {
-            if (req.body.userName != username) {
-                // alert!!!
+            if (req.body.userName !== username) {
+                // alert logic here if needed
             }
             res.status(200).json({
                 status: "OK",
                 userName: username,
                 message: "Welcome, §p" + username + "§r, your account has been verified §asucessfully§r"
-            })
+            });
             return;
         }
 
         else if (accessCode === "Suspended") {
-            if (req.body.userName != username) {
-                // alert!!!
+            if (req.body.userName !== username) {
+                // alert logic here if needed
             }
             res.status(200).json({
                 status: "Suspended",
                 userName: username,
                 message: "Welcome, §p" + username + "§r, your account has been §4suspended§r and you cannot use this application"
-            })
+            });
             return;
         }
 
         else if (accessCode === "EasterEgg") {
-            console.log("Easter egg with code \"" + req.query.key + "\" was triggered")
+            console.log("Easter egg with code \"" + userKey + "\" was triggered");
             res.status(200).json({
                 status: "EasterEgg",
-                userName: req.query.key,
+                userName: userKey,
                 message: username
-            })
+            });
             return;
         }
 
     } else {
-        console.log("Someone tried to log in with an invalid key [" + req.ip + "]")
+        console.log("Someone tried to log in with an invalid key [" + req.ip + "]");
         res.status(200).json({
             status: "InvalidAccount",
             message: "§4Invalid§r access key"
-        })
+        });
         return;
     }
-    console.error("i dunno what happened OwO [" + req.ip + "]")
+    console.error("i dunno what happened OwO [" + req.ip + "]");
     res.status(500).json({ status: "ServerError", message: "§4Bad request, unknown error§r" });
-    return;
 }
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Miliduper Backend V1\nServer is running on port ${port}`);
+    console.log(`MiliDuper Backend V2.1\nServer is running on port ${port}`);
 });
